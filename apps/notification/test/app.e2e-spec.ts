2fa1587eb48da3747d9993ea61dfa48df2f0daa9
israel <debanjo31@gmail.com>
2025-10-07 23:47:01 +0100

    feat: implement user and bid modules with controllers, services, and tests

--- apps/notification/test/app.e2e-spec.ts
+++ apps/notification/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AuthModule } from './../src/auth.module';
+import { NotificationModule } from './../src/notification.module';
 
-describe('AuthController (e2e)', () => {
+describe('NotificationController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule],
+      imports: [NotificationModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
