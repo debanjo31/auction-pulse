0034eb4e738ae47578bae95f74aaece7066b1ee9
israel <debanjo31@gmail.com>
2025-10-24 00:54:48 +0100

    feat: initialize auction-ms, notification-ms, and wallet-ms applications with NestJS structure
    - Added main application files including controllers, services, and modules for auction-ms, notification-ms, and wallet-ms.
    - Implemented basic GET endpoint returning a message "Hello API".
    - Set up Jest testing framework with initial test cases for controllers and services.
    - Configured TypeScript settings for each application with appropriate tsconfig files.
    - Integrated ESLint configuration for code quality checks.
    - Established Webpack configuration for building applications.
    - Created end-to-end test setup for notification-ms and wallet-ms applications.

---
+++ apps/auction-ms/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'auction-ms',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/auction-ms',
+};
