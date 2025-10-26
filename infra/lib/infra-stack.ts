1597acba254c15fafd1870e482ccec5e0179cd91
israel <debanjo31@gmail.com>
2025-10-26 16:56:58 +0100

    feat: add S3 bucket for auction assets and create .gitkeep for assets directory

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const auctionBucket = new s3.Bucket(this, 'AuctionBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: true,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      }),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, 'AuctionBucketName', {
+      value: auctionBucket.bucketName,
+      description: 'The name of the S3 bucket',
+      exportName: 'AuctionBucketName',
+    });
   }
 }
